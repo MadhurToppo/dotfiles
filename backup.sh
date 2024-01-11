8bf9bace7075b203b1c9332d23ab7ba06f39264e
Madhur Toppo <madhur.toppo@gmail.com>
2024-01-12 02:00:55 +0530

    Create backup of .zshrc and .spacemacs

--- backup.sh
+++ backup.sh
@@ -31,7 +31,7 @@ sed '/^[ \t]*$/d' $backupPaths | while read filePath; do
   originalFile="${filePath//${findThis}/${replaceWith}}"
 
   # copying the files
-  cp --parents --recursive $originalFile ./configurations
+  cp -R $originalFile ./configurations
   sleep 0.05
 done
 
